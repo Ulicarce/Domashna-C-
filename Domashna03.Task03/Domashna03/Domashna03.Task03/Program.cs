98fb66d0fd18feb23be252f5f0f7178e65972222
Tatjana Ristovska <96683871+Ulicarce@users.noreply.github.com>
2023-03-11 15:10:43 +0100

    Add files via upload

---
+++ Domashna03.Task03/Domashna03/Domashna03.Task03/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Ve molam vnesete broj:");
+bool NumberParsed = int.TryParse(Console.ReadLine(), out int NumbersParsed);
+if (NumberParsed)
+{
+    Console.WriteLine($"Zbirot e: {SumOfDigits(NumbersParsed)}");
+}
+else
+{
+    Console.WriteLine("Greshka, Ve molam vnesete broj!");
+}
+int SumOfDigits(int inputNumber)
+{
+    int sumOfDigits = 0;
+    while (inputNumber > 0)
+    {
+        sumOfDigits += inputNumber % 10;
+        inputNumber /= 10;
+    }
+    return sumOfDigits;
+}
