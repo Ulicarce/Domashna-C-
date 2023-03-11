ce702bcee0c2c3477ccc109a0a85ceead57438d9
Tatjana Ristovska <96683871+Ulicarce@users.noreply.github.com>
2023-03-11 15:09:51 +0100

    Add files via upload

---
+++ Domashna03.Task02/Domashna03/Domashna03.Task02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine();
+Console.WriteLine("Ve molam vnesete rechenica:");
+string inputSentence = Console.ReadLine();
+string[] sentenceWords = inputSentence.Split(" ");
+Console.WriteLine("Vashata rechenica e:");
+foreach (string word in sentenceWords)
+{
+    Console.WriteLine($"{word} ");
+}
