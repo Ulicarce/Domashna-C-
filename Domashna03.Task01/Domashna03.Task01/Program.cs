4737aafc730857783a54583a0808732b85580b39
Tatjana Ristovska <96683871+Ulicarce@users.noreply.github.com>
2023-03-12 15:49:54 +0100

    Add files via upload

---
+++ Domashna03.Task01/Domashna03.Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Vnesete podatok:");
+string input = Console.ReadLine();
+
+if (input.Length >= 5)
+{
+    string lastFive = input.Substring(input.Length - 5);
+    Console.WriteLine("Posledni 5 karakteri se: " + lastFive);
+}
+else
+{
+    Console.WriteLine("Vnesovte pomalku od 5karakteri!");
+}
